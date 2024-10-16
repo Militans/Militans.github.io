2492f8a3d89eff8c90185901f4884e882d4b3297
Militans <ivangreku48@gmail.com>
2024-10-16 14:48:58 +0300

    changing nltk.setup

--- nltk_setup.py
+++ nltk_setup.py
@@ -2,7 +2,14 @@ import nltk
 from nltk.corpus import stopwords
 import os
 
-
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+try:
+    nltk.data.find('corpora/words')
+except LookupError:
+    nltk.download('punkt')
 if not os.path.exists(nltk.data.find('corpora/stopwords')):
     nltk.download('stopwords')
 if not os.path.exists(nltk.data.find('tokenizers/punkt')):
